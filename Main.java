f810beaf137305bde79a2d071881bc62a47b8c16
bego <begomontes7@gmail.com>
2023-03-13 08:57:09 +0100

    Finished extra functionality

--- Main.java
+++ Main.java
@@ -2,30 +2,35 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
 
-        //functionality related to displaying fake data about past players of the game
-        ArrayList<FakePerson> people = new ArrayList<FakePerson>();
-        Random r = new Random();
-        String names [] = {"Bego", "Pablo", "Juan", "Maria", "Pedro"};
-        
+        //creates the array of fake people
+        createFakePeople();
+        //starts the Warchest game
+        Game newGame = new Game();
+        newGame.startGame();
+    }
 
-        System.out.println("####PLAYERS THAT HAVE WON WARCHEST###");
-        System.out.println();
+    //functionality related to displaying fake data about past players of the game and sorting them by date
+    static void createFakePeople() {
+         ArrayList<FakePerson> people = new ArrayList<FakePerson>();
+         Random r = new Random();
+         String names [] = {"Bego", "Pablo", "Juan", "Maria", "Sofia"};
+         
+ 
+         System.out.println("####PLAYERS THAT HAVE WON WARCHEST###");
+         System.out.println();
+ 
+         for (int i = names.length-1; i >= 0; i--) {
+             String name = names[i];
+             int gamesWon = r.nextInt(10);
+             String date = "202"+i + "-" +(r.nextInt(0, 13)) + "-" + (r.nextInt(0,31));
+             FakePerson person = new FakePerson(name, gamesWon, date);
+             people.add(person);
+         }
 
-        for (int i = 0; i < names.length; i++) {
-            String name = names[i];
-            int gamesWon = r.nextInt(10);
-            String date = "2021-" + (r.nextInt(12) + 1) + "-" + (r.nextInt(28) + 1);
-            FakePerson person = new FakePerson(name, gamesWon, date);
-            people.add(person);
-        }
+        people.sort((p1, p2)-> p1.getDate().compareTo(p2.getDate()));
 
         for(FakePerson person: people){
             System.out.println(person.getName()+ " "+ person.getGamesWon()+ " " + person.getDate());
         }
-
-        //starts the Warchest game
-        Game newGame = new Game();
-        newGame.startGame();
-        
     }
 }
